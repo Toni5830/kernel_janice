0257f4071fcc4d2b89e981f7d73a2b665ec13b7b
Toni5830 <enrit96@gmail.com>
2013-12-26 14:59:21 +0100

    3.0.33->3.0.34

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Wed Aug 27 14:11:30 CEST 2014"
+#define LINUX_COMPILE_BY "tonello"
+#define LINUX_COMPILE_HOST "Intel-iMac"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
