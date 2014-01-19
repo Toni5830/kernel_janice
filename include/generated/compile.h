44fc935ac08e240d9d9647e934c3fdef91947a3f
Toni5830 <enrit96@gmail.com>
2014-01-19 11:24:16 +0100

    3.0.45->3.0.55

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Thu Jan 2 10:18:41 CET 2014"
+#define UTS_VERSION "#12 SMP PREEMPT Sun Jan 19 11:20:06 CET 2014"
 #define LINUX_COMPILE_BY "Toni5830"
 #define LINUX_COMPILE_HOST "iMac-Tonello.local"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
