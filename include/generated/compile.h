51c6634b085509454ff3ab3f00730d6a197461c5
Toni5830 <enrit96@gmail.com>
2014-01-27 15:41:30 +0100

    adb on startup(?)

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 14 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#14 SMP PREEMPT Sun Jan 26 18:48:38 CET 2014"
-#define LINUX_COMPILE_BY "Toni5830"
-#define LINUX_COMPILE_HOST "iMac-Tonello.local"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
