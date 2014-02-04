59d418565609a592873c10c52239b5c3d214cf49
Toni5830 <enrit96@gmail.com>
2014-02-04 16:41:46 +0100

    re,ove useless files

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 8 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Fri Jan 31 14:43:59 CET 2014"
-#define LINUX_COMPILE_BY "FoToni"
-#define LINUX_COMPILE_HOST "iMac-Tonello.local"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
